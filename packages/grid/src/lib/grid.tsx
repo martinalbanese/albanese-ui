97c38a90107c4f9f58b584cd5e2dc7615970fc84
martinalbanese <albanesemartinax@gmail.com>
2025-01-14 17:11:22 +0100

    feat: inizializzata library componente Grid

---
+++ packages/grid/src/lib/grid.tsx
@@ -0,0 +1,11 @@
+import styles from './grid.module.css';
+
+export function Grid() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Grid!</h1>
+    </div>
+  );
+}
+
+export default Grid;
